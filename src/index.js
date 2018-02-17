60055fde965cc5ce0bdcbb02d082b2ba3c37094a
User <User@Users-MacBook.local>
2018-02-17 21:57:40 +0000

    Add angular-route setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 
-const accApp = angular.module('accApp', []);
+const accApp = angular.module('accApp', ['ngRoute']);
+
+accApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl : "src/templates/main.html"
+  })
+  .when("/alt1", {
+    templateUrl : "src/templates/alt1.html"
+  })
+  .when("/alt2", {
+    templateUrl : "src/templates/alt2.html"
+  });
+  $locationProvider.html5Mode(true);
+});;
