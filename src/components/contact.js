bf23c832c00538caf4df0de7de7d8677dae68427
Ephraim <Ephraim@Aiyanas-MacBook-Air.local>
2018-03-06 12:48:32 +0000

    Contact form is validated, sanitized, and POSTs to /form

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,7 +2,41 @@ accApp.component('contact', {
   bindings: {
 
   },
-  controller: function() {
+  controller: function($http, $timeout) {
+  	this.formName = '';
+  	this.formEmail = '';
+  	this.formMessage = '';
+  	this.success = false;
+  	this.failure = false;
+  	this.onSubmit = function(contactForm) {
+  		console.log(contactForm);
+  		var config = {
+  			method: 'POST',
+  			url: '/form',
+  			data: {
+  				name: this.formName,
+  				email: this.formEmail,
+  				message: this.formMessage
+  			}
+  		}
+  		$http(config).then(response => { 
+  			this.success = true;
+  			contactForm.$setValidity(null, false);
+  			contactForm.$setPristine();
+  			contactForm.$setUntouched();
+  			$timeout(() => {
+			  	this.formName = '';
+			  	this.formEmail = '';
+				this.formMessage = '';
+  				this.success = false;
+  			}, 1500);
+  		}, error => {
+  			this.failure = true;
+  			$timeout(() => { 
+  				this.failure = false 
+  			}, 1500);
+  		});
+  	}
   },
   templateUrl: 'src/templates/contact.html'
 });
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,26 @@
 
 require('dotenv').config();
+var bodyParser = require('body-parser');
 var path = require('path');
 var express = require('express');
+var expressSanitized = require('express-sanitize-escape');
 var app = express();
 
 var root = path.join(__dirname, '/../../');
 
 app.use(express.static(root));
+app.use(bodyParser.json());
+app.use(expressSanitized.middleware());
 
-app.get('*', function(res, res) {
+app.get('*', function(req, res) {
 	res.sendFile(path.join(root, 'index.html'));
 });
 
+app.post('/form', function(req, res) {
+	console.log('Body is:', req.body);
+	res.sendStatus(201);
+});
+
 app.listen(process.env.PORT, function() {
-	console.log('Server running on port 3000')	
+	console.log('Server running on port 5000')	
 });
