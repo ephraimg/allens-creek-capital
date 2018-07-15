fec83257c41afa610c8a9e74917325a3e0217121
ephraimg <ephraim.glick@gmail.com>
2018-07-15 05:18:36 -0700

    Improve styling, add hamburger icon

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,6 +3,15 @@ accApp.component('navbar', {
 
   },
   controller: function() {
+    this.viewWidth = () => Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    this.menuOpen = this.viewWidth() > 768;
+    this.toggleMenu = function() {
+        // this.viewWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        // if (this.viewWidth <= 768) {
+            this.menuOpen = !this.menuOpen;
+        // }
+    };
+    this.check = () => this.menuOpen || this.viewWidth() > 768;
   },
   templateUrl: 'src/templates/navbar.html'
 });
