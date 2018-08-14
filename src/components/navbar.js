be12b662c31dc59eb45f2b37cc6796fb38ab20ac
ephraimg <ephraim.glick@gmail.com>
2018-08-14 00:08:41 -0700

    Refactor responsive design with display: none

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,13 +3,16 @@ accApp.component('navbar', {
 
   },
   controller: function() {
-    this.viewWidth = function() {
-        var clientWidth = document.documentElement.clientWidth;
-        return Math.max(clientWidth, window.innerWidth || 0);
-    };
-    this.menuOpen = this.viewWidth() > 768;
-    this.toggleMenu = function() { this.menuOpen = !this.menuOpen; };
-    this.check = function() { return this.menuOpen || this.viewWidth() > 768; };
+
+    this.menuClass = 'closed';
+    this.closeMenu = function() {
+      this.menuClass = 'closed';
+    }
+    this.openMenu = function($event) {
+      $event.stopPropagation();
+      this.menuClass = 'open';
+    }
+
   },
   templateUrl: 'src/templates/navbar.html'
 });
